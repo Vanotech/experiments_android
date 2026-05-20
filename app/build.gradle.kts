995ec50687b46f4f0373651ec7e561f067f4ac5f
Jakes Mo <jakes.mo@hotmail.com>
2026-05-20 12:16:28 +0100

    refactor: simpify Navigator

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -71,6 +71,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.viewmodel.compose)
+    implementation(libs.androidx.lifecycle.viewmodel.navigation3)
     implementation(libs.androidx.navigation3.runtime)
     implementation(libs.androidx.navigation3.ui)
     implementation(libs.coil.network.ktor3)
